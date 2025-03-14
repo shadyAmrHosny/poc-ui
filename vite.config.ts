4ee03477a491a2ebd08c83dd309235de1c3e9fbf
shadyAmrHosny <shadyamrhosny@gmail.com>
2025-03-14 13:42:28 +0200

    fixing some bugs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 //const PORT = Number(process.env.PORT) || 4000
@@ -12,8 +11,8 @@ export default defineConfig(({ mode }) => ({
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === 'development'
+    //&& componentTagger(),
   ].filter(Boolean),
   resolve: {
     alias: {
